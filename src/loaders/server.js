5b7d88f366967be8473ff7e789975689e81f464b
Anhw <rktsinger@gmail.com>
2022-10-16 01:44:30 +0900

    feat: 프로젝트 디렉토리 구조 설정 및 mongoose 세팅
    디렉토리 구조 설정 후 기존 파일들 위치 변경
    database.js 추가
    package.json 의존성 변경

---
+++ src/loaders/server.js
@@ -0,0 +1,61 @@
+const http = require("http");
+const logger = require("../../libs/logger");
+
+const loadHttpServer = (app) => {
+  const normalizePort = (val) => {
+    const port = parseInt(val, 10);
+
+    if (Number.isNaN(port)) {
+      return val;
+    }
+
+    if (port >= 0) {
+      return port;
+    }
+
+    return false;
+  };
+
+  const port = normalizePort(process.env.PORT || "8080");
+  app.set("port", port);
+
+  const server = http.createServer(app);
+
+  server.listen(port);
+
+  const onError = (error) => {
+    if (error.syscall !== "listen") {
+      throw error;
+    }
+
+    const bind = typeof port === "string" ? `Pipe ${port}` : `Port ${port}`;
+
+    switch (error.code) {
+      case "EACCES": {
+        logger.error(`${bind} requires elevated privileges`);
+        process.exit(1);
+        break;
+      }
+      case "EADDRINUSE": {
+        logger.error(`${bind} is already in use`);
+        process.exit(1);
+        break;
+      }
+      default: {
+        throw error;
+      }
+    }
+  };
+
+  const onListening = () => {
+    const addr = server.address();
+    const bind =
+      typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
+    logger.debug(`Listening on ${bind}`);
+  };
+
+  server.on("error", onError);
+  server.on("listening", onListening);
+};
+
+module.exports = loadHttpServer;
