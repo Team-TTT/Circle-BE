371d7002a6f574355a32be13fa449f0a99aa74ff
csy <csl8804@gmail.com>
2022-10-16 20:13:55 +0900

    feat: db schema
    User, Project, Channel Schema 작성

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  displayName: { type: String, required: true },
+  profile_url: { type: String },
+  projects: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+    },
+  ],
+});
+
+module.exports = mongoose.model("User", userSchema);
