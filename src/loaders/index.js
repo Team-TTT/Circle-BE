30f0e077d1db8f7f1a9ac56f9b18d741987871d0
Yanguk <heo.yanguk@gmail.com>
2022-10-16 16:46:00 +0900

    refactor: 마지막 에러 모아서 처리 하는방식 간략화
    필요없는 res.local 객체에 넣는 에러 메세지 삭제

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -23,9 +23,6 @@ const initLoaders = async (app) => {
   });
 
   app.use((err, req, res, next) => {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
-
     res.status(err.status || 500).json(err);
   });
 
