691e38bcf4d2de100df97e003cd75b0f4d5cbafa
Yanguk <heo.yanguk@gmail.com>
2022-10-20 15:05:25 +0900

    refactor: userId -> providerId로 변경
    findById -> findOne({ providerId })로 변경하였습니다.
    에러 logger를 마지막 에러 모아서 처리하는 곳에서 로그내역을 남겼습니다.

--- models/User.js
+++ models/User.js
@@ -18,6 +18,7 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     unique: true,
+    index: true,
     required: true,
     validate: [validateEmail, MESSAGE.INVALID_EMAIL],
   },
