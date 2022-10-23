9f94ad6bb7ff98148ed74c15157d6492766a8829
Choi Song Yi <95201149+ssong-yi@users.noreply.github.com>
2022-10-23 23:09:37 +0900

    [2022.10.23] feat: channels post api (#19)
    * refactor: user 정보를 불러오는 방법 변경
    
    - email이 아닌 google에서 제공하는 고유한 uid값을 받아서 DB의 User에서 불러올 때 사용.
    
    - 위 방법으로 변경함에 따라 email의 index 설정을 없애줌
    - 사용하지 않는 logger 삭제
    
    * feat: channels post api 구현
    
    - channelSchema에 _id 추가
    - project router에 추가
    - channel controller에 createChannel로 채널 등록
    
    * version 업데이트
    
    * Apply suggestions from code review
    
    createChannel에 session.endSession() 추가
    
    Co-authored-by: Yanguk <99335782+Yanguk@users.noreply.github.com>
    
    Co-authored-by: csy <csl8804@gmail.com>
    Co-authored-by: Yanguk <99335782+Yanguk@users.noreply.github.com>

--- models/Channel.js
+++ models/Channel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const channelSchema = new mongoose.Schema({
+  _id: { type: mongoose.Types.ObjectId },
   title: {
     type: String,
     required: true,
--- src/routes/project.route.js
+++ src/routes/project.route.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const projectController = require("./controllers/project.controller");
+const channelController = require("./controllers/channel.controller");
 
 router
   .route("/")
@@ -14,5 +15,9 @@ router
   .get(projectController.getProject)
   .put(projectController.editProject)
   .delete(projectController.deleteProject);
+
+router
+  .route("/:projectId/channels")
+  .post(channelController.createChannel);
 
 module.exports = router;
