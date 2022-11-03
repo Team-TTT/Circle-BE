c8e9fded88c6a5345d87750813993fdf388fda3c
Yanguk <heo.yanguk@gmail.com>
2022-11-03 11:45:29 +0900

    refactor: 코드리뷰 반영

--- spec/02-projectRouter.js
+++ spec/02-projectRouter.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const mongoose = require("mongoose");
 const request = require("supertest");
 const express = require("express");
@@ -11,57 +12,46 @@ const ProjectModel = require("../models/Project");
 let server = null;
 
 describe("* projects Router", () => {
-  before(function(done) {
-    initAsyncApp(express()).then(async (app) => {
-      server = request.agent(app);
-      await testDbConnecting(process.env.MONGODB_TEST);
-      done();
-    });
+  before(async () => {
+    const app = await initAsyncApp(express());
+    server = request.agent(app);
+
+    return testDbConnecting(process.env.MONGODB_TEST);
   });
 
-  after(function(done) {
-    mongoose.disconnect().then(done);
-  });
+  after(() => mongoose.disconnect());
 
   let projectId = "";
 
-  it("post /projects", (done) => {
+  it("post /projects", async () => {
     const newProject = {
       title: "test",
     };
 
-    server
+    const response = await server
       .post("/projects")
       .set("Cookie", `session=${process.env.COOKIE}`)
       .send(newProject)
-      .expect(200)
-      .end((err, res) => {
-        projectId = res.body.id;
-        done();
-      })
+      .expect(200);
+
+    projectId = response.body.id;
   });
 
-  it("Get /projects", (done) => {
-    server
+  it("Get /projects", async () => {
+    const response = await server
       .get("/projects")
       .set("Cookie", `session=${process.env.COOKIE}`)
-      .expect(200)
-      .end((err, res) => {
-        if (err) {
-          return err;
-        }
+      .expect(200);
 
-        const targetProject = res.body
-          .map((project) => project._id)
-          .find((id) => id === projectId);
+    const targetProject = response.body
+      .map((project) => project._id)
+      .find((id) => id === projectId);
 
-        expect(!!targetProject).to.be.true;
-        done();
-      });
+    expect(!!targetProject).to.be.true;
   });
 
   it("Put /projects/:projectId", async () => {
-    const newTitle = { title: "test2" }
+    const newTitle = { title: "test2" };
 
     await server
       .put(`/projects/${projectId}`)
@@ -73,18 +63,12 @@ describe("* projects Router", () => {
     expect(targetProject.title).to.equal(newTitle.title);
   });
 
-  it("Delete /projects/:projectId", (done) => {
-    server
+  it("Delete /projects/:projectId", async () => {
+    const response = await server
       .delete(`/projects/${projectId}`)
       .set("Cookie", `session=${process.env.COOKIE}`)
-      .expect(200)
-      .end((err, res) => {
-        if (err) {
-          return err;
-        }
+      .expect(200);
 
-        expect(res.body).to.eql({ result: "success" });
-        done();
-      });
+    expect(response.body).to.eql({ result: "success" });
   });
 });
