0fccfbf3c710491178dd820e034acc83076baafd
Yanguk <heo.yanguk@gmail.com>
2022-11-03 13:12:18 +0900

    refactor: db연결 async로 변경

--- src/loaders/database.js
+++ src/loaders/database.js
@@ -2,29 +2,25 @@ const mongoose = require("mongoose");
 
 const logger = require("../../libs/logger");
 
-const loadDatabase = () => new Promise((res, rej) => {
-  mongoose.connection.on("error", (error) => {
-    logger.error(`Connection error: ${error.toString()}`);
-  });
+const loadDatabase = async () => {
+  try {
+    mongoose.connection.on("error", (error) => {
+      logger.error(`Connection error: ${error.toString()}`);
+    });
 
-  mongoose.connection.once("disconnected", () => {
-    logger.info("MongoDB disconnected");
-  });
+    mongoose.connection.once("disconnected", () => {
+      logger.info("MongoDB disconnected");
+    });
 
-  mongoose.connect(
-    process.env.MONGODB_SECRET_STRING,
-    { dbName: process.env.MONGODB_DB_NAME },
-    (error) => {
-      if (error) {
-        logger.error(`Connecting error: ${error.toString()}`);
+    await mongoose.connect(process.env.MONGODB_SECRET_STRING, {
+      dbName: process.env.MONGODB_DB_NAME,
+    });
 
-        rej(new Error("MongoDB initial connection failed"));
-      } else {
-        logger.info("Successfully connected to mongoDB");
-        res();
-      }
-    },
-  );
-});
+    logger.info("Successfully connected to mongoDB");
+  } catch (error) {
+    logger.error(`Connecting error: ${error.toString()}`);
+    throw new Error("MongoDB initial connection failed");
+  }
+};
 
 module.exports = loadDatabase;
