ec8cd699616c7b66b427f46375641992e1a035ad
Yanguk <99335782+Yanguk@users.noreply.github.com>
2022-11-05 10:43:44 +0900

    test: projectRouter 테스트 코드 작성 (#31)
    * test/tempo: b6ba648 test: projectRouter 테스트 코드 작성
    
    * test: projectRouter 테스트 코드 작성
    
    * refactor: 코드리뷰 반영
    
    * refactor: db삭제 메소드 추가

---
+++ spec/02-projectRouter.js
@@ -0,0 +1,77 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const request = require("supertest");
+const express = require("express");
+const { expect } = require("chai");
+
+const initAsyncApp = require("../src/loaders/initAsyncApp");
+const { testDbConnect } = require("./utils");
+const ProjectModel = require("../models/Project");
+
+let server = null;
+
+describe("* projects Router", () => {
+  before(async () => {
+    const app = await initAsyncApp(express());
+    server = request.agent(app);
+
+    await testDbConnect();
+  });
+
+  after(async () => {
+    await mongoose.connection.db.dropDatabase();
+    await mongoose.disconnect();
+  });
+
+  let projectId = "";
+
+  it("post /projects", async () => {
+    const newProject = {
+      title: "test",
+    };
+
+    const response = await server
+      .post("/projects")
+      .set("Cookie", `session=${process.env.COOKIE}`)
+      .send(newProject)
+      .expect(200);
+
+    projectId = response.body.id;
+  });
+
+  it("Get /projects", async () => {
+    const response = await server
+      .get("/projects")
+      .set("Cookie", `session=${process.env.COOKIE}`)
+      .expect(200);
+
+    const targetProject = response.body
+      .map((project) => project._id)
+      .find((id) => id === projectId);
+
+    expect(!!targetProject).to.be.true;
+  });
+
+  it("Put /projects/:projectId", async () => {
+    const newTitle = { title: "test2" };
+
+    await server
+      .put(`/projects/${projectId}`)
+      .set("Cookie", `session=${process.env.COOKIE}`)
+      .send(newTitle)
+      .expect(200);
+
+    const targetProject = await ProjectModel.findById(projectId).lean().exec();
+    expect(targetProject.title).to.equal(newTitle.title);
+  });
+
+  it("Delete /projects/:projectId", async () => {
+    const response = await server
+      .delete(`/projects/${projectId}`)
+      .set("Cookie", `session=${process.env.COOKIE}`)
+      .expect(200);
+
+    expect(response.body).to.eql({ result: "success" });
+  });
+});
